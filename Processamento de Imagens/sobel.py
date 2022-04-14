3951a8929bf7c0ce6546151c265e8ed98156ca37
MullerIsaac <isaacmuller321@gmail.com>
2022-04-14 16:12:54 -0300

    sobel implementado

---
+++ Processamento de Imagens/sobel.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def convolution(image, kernel, average=False):
+    if len(image.shape) == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+    image_row, image_col = image.shape
+    kernel_row, kernel_col = kernel.shape
+
+    output = np.zeros(image.shape)
+
+    pad_height = int((kernel_row - 1) / 2)
+    pad_width = int((kernel_col - 1) / 2)
+
+    padded_image = np.zeros((image_row + (2 * pad_height), image_col + (2 * pad_width)))
+
+    padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
+
+
+    for row in range(image_row):
+        for col in range(image_col):
+            output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
+            if average:
+                output[row, col] /= kernel.shape[0] * kernel.shape[1]
+
+    return output
+
+def sobel_filter(image, filter, name):
+    new_image = convolution(image, filter)
+ 
+    cv2.imwrite(name, new_image)
+ 
+ 
+if __name__ == '__main__':
+    filter = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+
+    image = plt.imread('macacoBlured.jpg')
+    
+    sobel_filter(image, filter, "macacoSobely.jpg")
+
+    sobel_filter(image, np.flip(filter.T, axis=0), "macacoSobelx.jpg")
